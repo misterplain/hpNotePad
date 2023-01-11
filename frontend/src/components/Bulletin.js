619036dd8374055ece11aeff7e63a47c6b287736
Patrick O'Brien <obrienp2321@gmail.com>
2023-01-11 19:31:03 +0100

    doa extenions button

--- frontend/src/components/Bulletin.js
+++ frontend/src/components/Bulletin.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Box, Paper, Grid, Typography } from "@mui/material";
+import { Box, Paper, Grid, Typography, Button } from "@mui/material";
 
 const Bulletin = () => {
   return (
-    <Grid container justifyContent="center">
+    <Grid container justifyContent='center'>
       <Grid xs={12} sm={10} md={8} marginBottom>
         {" "}
         <Box
@@ -13,11 +13,10 @@ const Bulletin = () => {
             height: "100%",
             justifyContent: "center",
             alignItems: "center",
-            border: "1px solid red",
             borderRadius: "10px",
           }}
         >
-          <Typography variant='h6' padding>
+          {/* <Typography variant='h6' padding>
            Change of Mind / DOA Holiday returns extension: 
            <ul>
             <li>Invoiced Nov 25, return end date Jan 31 - returns window 67 days</li>
@@ -25,7 +24,20 @@ const Bulletin = () => {
             <li>....and so on...</li>
             <li>Invoiced Dec 31, return end date Jan 31 - returns window 31 days</li>
            </ul>
-          </Typography>
+          </Typography> */}
+          <a
+            href='https://docs.google.com/spreadsheets/d/18jm6P70TQOF6WZvY_TfGw3a0CKxvfZigiV73o9GPlKs/edit?usp=sharing'
+            target='__blank'
+            style={{ textDecoration: "none" }}
+          >
+            <Button
+              variant='contained'
+              color='secondary'
+              sx={{ textDecoration: "none" }}
+            >
+              DOA/Change of Mind Extension Timeline
+            </Button>
+          </a>
         </Box>
       </Grid>
       {/* <Grid xs={12} sm={10} md={8} gutterBottom>
