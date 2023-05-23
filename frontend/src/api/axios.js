d0057fae3f057f55471e1a79c7d4232eff1c1350
Patrick O'Brien <obrienp2321@gmail.com>
2023-05-23 08:28:30 +0200

    baseURL for axios to cyclic from render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://activeserver.onrender.com/notepad",
+  // baseURL: "https://activeserver.onrender.com/notepad",
+baseURL: "https://cute-gray-pelican-tux.cyclic.app/notepad"
   // baseURL: "http://localhost:5000/notepad",
 });
+``
