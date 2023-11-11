5ae0982f210bca2d91ff8f91156e65bc60d8af01
Patrick O'Brien <obrienp2321@gmail.com>
2023-11-11 18:05:01 +0100

    move api baseLine back to cyclic

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: "https://cute-gray-pelican-tux.cyclic.app/notepad",
-// });
-
 export default axios.create({
-  baseURL: "https://activeserver.onrender.com/notepad",
+  baseURL: "https://cute-gray-pelican-tux.cyclic.app/notepad",
 });
+
+// export default axios.create({
+//   baseURL: "https://activeserver.onrender.com/notepad",
+// });
 
 // baseURL: "https://activeserver.onrender.com/notepad",
 // baseURL: "http://localhost:5000/notepad",
