6d33c4674e40c5c85164c5a09b8fee9e498dc8e7
Patrick O'Brien <obrienp2321@gmail.com>
2023-12-27 20:52:32 +0100

    make post/contact an async function

--- server/routes/contactRoute.js
+++ server/routes/contactRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const nodemailer = require("nodemailer");
 const nodeMailerConfirmationEmail = require("../utils/nodeMailer");
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { result, name, email, message, phoneNum } = req.body;
   const { source } = req.query;
 
@@ -11,8 +11,10 @@ router.post("/", (req, res) => {
   const test = nodeMailerConfirmationEmail(source, req.body);
 
   try {
-    nodeMailerConfirmationEmail(source, req.body);
-    return res.status(200).json({ message: "Message sent successfully" });
+    const emailSent = await nodeMailerConfirmationEmail(source, req.body);
+    return res
+      .status(200)
+      .json({ message: "Message sent successfully" + emailSent });
   } catch (error) {
     nodeMailerConfirmationEmail(source, { message: "Failure to send message" });
     return res.status(500).json({ message: "Error sending message" });
