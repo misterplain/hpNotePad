c19014f44bb24a488093d5d51b1bea7d949aaba5
Patrick <obrienp2321@gmail.com>
2025-10-30 18:17:05 +0100

    move server api into env file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+
 export default axios.create({
-  baseURL: "https://node-server-4m2h.onrender.com/hpnotepad"
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.SERVER_API_URL
+      : "http://localhost:5000/hpnotepad",
 });
-
-
-
