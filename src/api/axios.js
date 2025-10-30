6bd0256b90057c31513123efb53b3e5a1ac14288
Patrick <obrienp2321@gmail.com>
2025-10-30 18:30:47 +0100

    env variable renaming to align with CRA standards

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.SERVER_API_URL
+      ? process.env.REACT_APP_SERVER_API_URL
       : "http://localhost:5000/hpnotepad",
 });
